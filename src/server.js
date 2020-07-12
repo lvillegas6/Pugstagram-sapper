eccc203b09c545e553f2c57f86e74011c9a338e6
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-07-12 12:00:00 +0100

    Server and Client

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import sirv from 'sirv';
+import compression from 'compression';
+import polka from 'polka';
+import * as sapper from '@sapper/server';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+polka()
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
