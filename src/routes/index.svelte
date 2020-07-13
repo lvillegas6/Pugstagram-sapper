fc6c5c6167f35270fd1b066100cdea01c3598f2b
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-07-13 17:08:18 -0500

    Rutas

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export async function preload(){
+    let data = {}
+    const response = await this.fetch(
+      `https://us-central1-pugstagram-co.cloudfunctions.net/data`
+    )
+    data = await response.json()
+    return { data }
+  }
+</script>
+
+<script>
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  export let data
+
+</script>
+
+<svelte:head>
+  <title>Pugstagram</title>
+</svelte:head>
+
+<Main>
+    <TimeLine posts={data.posts} />
+    <Sidebar {...data.user} />
+</Main>
---
+++ src/routes/profile.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Main from '../components/Main.svelte'
+</script>
+
+<style>
+    .Profile {
+        padding: 4em 0 0 0;
+    }
+</style>
+
+<svelte:head>
+    <title>Configuracion del perfil</title>
+</svelte:head>
+
+<Main>
+    <div class="Profile">
+        <h2>Configuracion del perfil</h2>
+    </div>
+</Main>
